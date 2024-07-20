b41186fd85ef75ad76e7a193f53ef3299828e87f
Fabio Lopes <fabiolopes7844@gmail.com>
2024-07-19 22:28:02 -0300

    Created First Model

---
+++ LCD_and_DHT/webapi/Models/EnviromentModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace webapi.Models{
+    public class EnviromentModel {
+
+        [Key]
+        public long EnviromentId {get; set;}
+        public float Temperature {get; set;}
+        public float Humidity {get; set;}
+
+
+    }
+
+}
+
