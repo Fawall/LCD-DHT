a50574a3c0915f2eafeae76e70fb9ba9a737b1e3
Fabio Lopes <fabiolopes7844@gmail.com>
2024-07-21 18:53:12 -0300

    added datetime in database

---
+++ LCD_and_DHT/webapi/Migrations/20240720150353_AddingDateTimeColumn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDateTimeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "Enviroment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Enviroment");
+        }
+    }
+}
